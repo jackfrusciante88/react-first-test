2c69a1c377254b75c82c608f2a3dc97961226a4a
jackfrusciante88 <45917335+jackfrusciante88@users.noreply.github.com>
2020-05-06 00:02:19 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from "react";
-import "./styles.css";
+import logo from "./logo.png";
+import "./App.css";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
+
+export default App;
