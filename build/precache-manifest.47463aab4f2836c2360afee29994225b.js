370d628c32ae3e1cf919bdf07f947b13bf015041
Jack Frusciante <ciao@prova.com>
2020-05-14 22:36:08 +0200

    ok

---
+++ build/precache-manifest.47463aab4f2836c2360afee29994225b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f43ddc085eaeada763e8d943e2a7595e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9a9e7bd9bb8e1e6f706",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "08e223ae520e22d0b02b",
+    "url": "/static/css/main.29ac746d.chunk.css"
+  },
+  {
+    "revision": "a9a9e7bd9bb8e1e6f706",
+    "url": "/static/js/2.7652540f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.7652540f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08e223ae520e22d0b02b",
+    "url": "/static/js/main.5d71ad52.chunk.js"
+  },
+  {
+    "revision": "aa8618cdfcd70af43858",
+    "url": "/static/js/runtime-main.5022fdfd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
